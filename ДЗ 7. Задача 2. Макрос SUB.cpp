2edaad7a5af2cfc45a5b41cbc923f75e82eb79b4
Roman <romka200211@gmail.com>
2023-05-11 21:33:37 +0200

    first commit

---
+++ ДЗ 7. Задача 2. Макрос SUB.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#define SUB(A,B) ((A)-(B))
+
+int main()
+{
+    int a = 6;
+    int b = 5;
+    int c = 2;
+    std::cout << SUB(a,b)<<std::endl;
+    std::cout << SUB(a, b) * c << std::endl;
+    std::cout << SUB(a, b + c) * c << std::endl;
+}
+
